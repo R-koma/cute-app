591c6b26ed06c4a527564e6aeecb508939cc3df5
R-koma <ryomakomazawa@gmail.com>
2024-10-13 16:24:16 +0900

    apply background for output page

--- frontend/src/components/Result.jsx
+++ frontend/src/components/Result.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef, useEffect } from "react";
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import "../App.css";
 
 const Result = () => {
   const [inputText, setInputText] = useState(""); // 入力されたテキストを保持
